23c18897c1c7e0f7733ef87cec37913c65acc635
Jean García <116606354+Y34NxD@users.noreply.github.com>
2022-10-25 19:54:31 -0500

    Creación de repositorio para el proyecto

---
+++ src/proyectopoo/ProyectoPOO.java
@@ -0,0 +1,17 @@
+
+package proyectopoo;
+/**
+ *
+ * @author Equipo D
+ */
+public class ProyectoPOO {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("PROYECTO");
+        
+    }
+    
+}
